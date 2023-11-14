df46cb3c13e45754721076acd00be17dbb65cb88
Daniel Lindsley <daniel@toastdriven.com>
2023-11-14 09:16:28 -0600

    Minimal onMouseEnter/onMouseLeave examples for both React & Preact.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import './index.css';
+
+const allOptions = [
+  'One',
+  'Two',
+  'Three',
+];
 
 function App() {
+  const [activeOptions, setActiveOptions] = useState([]);
+
+  useEffect(() => {
+    console.log(`Active Options changed to: ${activeOptions.join(', ')}`);
+  }, [activeOptions]);
+
+  function handleSelect(optionName) {
+    const newOptions = activeOptions.slice();
+    newOptions.push(optionName);
+    setActiveOptions(newOptions);
+  }
+
+  function handleDeselect() {
+    const newOptions = [];
+    setActiveOptions(newOptions);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid grid-cols-2 gap-4">
+      <div>
+        <h2 className="font-bold text-2xl mb-2">All Options</h2>
+
+        <ul className="block">
+          {allOptions.map((optionName, offset) => (
+            <li
+              key={offset}
+              onMouseEnter={() => handleSelect(optionName)}
+              onMouseLeave={handleDeselect}
+            >
+              {optionName}
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div>
+        <h2 className="font-bold text-2xl mb-2">Active Options</h2>
+
+        <ul>
+          {allOptions.map((optionName, offset) => (
+            (activeOptions.indexOf(optionName) >= 0
+              ? (
+                <li
+                  key={offset}
+                  className="text-amber-500"
+                >
+                  {optionName}
+                </li>
+              )
+              : (
+                <li key={offset}>{optionName}</li>
+              )
+            )
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
